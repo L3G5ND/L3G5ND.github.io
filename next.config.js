19e3db5108266eb47f7e15a326b842272656fb6b
Malachi Vaillancourt <malachi_vaillancourt@icloud.com>
2024-07-28 15:06:13 -0400

    commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: "/github-portfolio",
     reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
