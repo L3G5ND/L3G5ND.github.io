90084d8ee57b96c303c54295dd77ae439b5e3623
Malachi Vaillancourt <malachi_vaillancourt@icloud.com>
2026-03-10 18:41:11 -0400

    commit

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -28,7 +28,7 @@ export default function About() {
                         With my experience developing in Roblox, I can confidently say, there isn&apos;t anything I cant do.
                         I have made a unique variety of <Link className={styles['text-link']}href="/portfolio">projects</Link>.
                         Consisting of cool gimiks you may find interesting, full open source libraries designed for Roblox development, and a game fully scripted by me!
-                        <a className={styles['text-link']} target="_blank" href="https://www.roblox.com/games/100772130331089/Money-Simulator"> Check it Out!</a>
+                        <a className={styles['text-link']} target="_blank" href="https://www.roblox.com/games/100772130331089/Money-Anarchy"> Check it Out!</a>
                     </p>
                     <p className={styles["main"]}>
                         If you are looking for someone to program something up to your expectations, you are in the right place.
